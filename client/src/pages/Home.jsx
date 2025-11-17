21e17617357afb05eea7fd5770c5744a3104172f
Sidsd110401 <sid.deshetti@gmail.com>
2025-11-17 22:38:06 +0530

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+        <div className='flex flex-col items-center justify-center min-h-screen bg-linear-to-br from-blue-200 to-purple-400'>
             <Navbar />
             <Header />
         </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const navigate = useNavigate()
     return (
-        <div>Login</div>
+        <div className='min-h-screen bg-linear-to-br from-blue-200 to-purple-400 px-6 sm:px-0'>
+            <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
+                <img src={assets.logo} onClick={() => navigate('/')} className='w-28 sm:w-32 cursor-pointer hover:scale-110 transition-all'/>
+            </div>
+            <div className='flex min-h-screen items-center justify-center'>
+                <div className='bg-slate-900 rounded-lg shadow-lg w-full text-sm text-indigo-300'>
+                    <h1 className='text-3xl text-center font-semibold text-white mb-5'>Login</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
